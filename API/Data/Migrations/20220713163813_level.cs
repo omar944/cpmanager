24cdda0814159e49dcb495b6fc456144dd1f0bb6
omar <omaralbitar77@gmail.com>
2022-07-13 19:38:49 +0300

    add level

---
+++ API/Data/Migrations/20220713163813_level.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class level : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Level",
+                table: "TrainingGroups",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "TrainingGroups");
+        }
+    }
+}
--- Entities/App/TrainingGroup.cs
+++ Entities/App/TrainingGroup.cs
@@ -4,6 +4,7 @@ namespace Entities.App;
 
 public class TrainingGroup : BaseEntity
 {
+    public string? Level { get; set; }
     public string? Name { get; set; }
     public List<TrainingGroupUser>? Students { get; set; }
     public User Coach { get; set; } = null!;
