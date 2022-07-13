c5791a936599765f3e13bad823c1ce2d099563ce
omar <omaralbitar77@gmail.com>
2022-07-13 23:06:43 +0300

    fix level

--- API/Models/TrainingGroupDto.cs
+++ API/Models/TrainingGroupDto.cs
@@ -5,4 +5,5 @@ public class TrainingGroupDto : BaseDto
     public string? Name { get; set; }
     public TeamUserDto? Coach { get; set; }
     public List<int>? Students { get; set; }
+    public string? Level { get; set; }
 }
